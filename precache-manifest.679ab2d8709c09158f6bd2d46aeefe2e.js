6dbba655a5aea95cd91f06808d15403635d5c8c5
Paulsonbigi <adegbokanpaul@gmail.com>
2020-10-09 14:42:17 +0100

    Updates

---
+++ precache-manifest.679ab2d8709c09158f6bd2d46aeefe2e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db52543eba85621586b7c29cb934b63e",
+    "url": "/shopping-using-context/index.html"
+  },
+  {
+    "revision": "7e084d2cb20f9e0265c9",
+    "url": "/shopping-using-context/static/css/main.c2fb3994.chunk.css"
+  },
+  {
+    "revision": "8ca0597e00843fc19653",
+    "url": "/shopping-using-context/static/js/2.cd02ba22.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-using-context/static/js/2.cd02ba22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e084d2cb20f9e0265c9",
+    "url": "/shopping-using-context/static/js/main.1f47c1a5.chunk.js"
+  },
+  {
+    "revision": "b85769937402eb221ed0",
+    "url": "/shopping-using-context/static/js/runtime-main.f110f830.js"
+  },
+  {
+    "revision": "3cfc1a798671dcd46455cdbd3e5d3d14",
+    "url": "/shopping-using-context/static/media/img1.3cfc1a79.png"
+  },
+  {
+    "revision": "d9206f288ba5b26c8d6c1bc9226ec4bb",
+    "url": "/shopping-using-context/static/media/img10.d9206f28.png"
+  },
+  {
+    "revision": "3ba15650aff95a34edd8f3c7f406d4bc",
+    "url": "/shopping-using-context/static/media/img12.3ba15650.png"
+  },
+  {
+    "revision": "3e6f471f66392045cbad019d1c2af209",
+    "url": "/shopping-using-context/static/media/img13.3e6f471f.png"
+  },
+  {
+    "revision": "7b1d1f28e60561ff1b12e2f26389ef67",
+    "url": "/shopping-using-context/static/media/img2.7b1d1f28.png"
+  },
+  {
+    "revision": "e7fe73cebce9c6fb06b420ed87b14c0c",
+    "url": "/shopping-using-context/static/media/img3.e7fe73ce.png"
+  },
+  {
+    "revision": "852cc0d22039b0d3bf0b38cf990c2720",
+    "url": "/shopping-using-context/static/media/img4.852cc0d2.png"
+  },
+  {
+    "revision": "dcf23a175cc9da3fe2a41d7153dd7094",
+    "url": "/shopping-using-context/static/media/img5.dcf23a17.png"
+  },
+  {
+    "revision": "b394c43e33f090d4b1bde9a8249f49a1",
+    "url": "/shopping-using-context/static/media/img6.b394c43e.png"
+  },
+  {
+    "revision": "e595da60fed80acb485e733387ef9487",
+    "url": "/shopping-using-context/static/media/img7.e595da60.png"
+  },
+  {
+    "revision": "98709bb90e7958315565304b3f6fd88a",
+    "url": "/shopping-using-context/static/media/img8.98709bb9.png"
+  },
+  {
+    "revision": "3c651af10a9cf6eb6754cba0cf5e9cbc",
+    "url": "/shopping-using-context/static/media/img9.3c651af1.png"
+  }
+]);
